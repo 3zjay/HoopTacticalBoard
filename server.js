f522870963b4648b6eb798da6a8a0d7344127d84
3zjay <isidro.pascua@gmail.com>
2026-04-25 08:59:22 -0400

    Modify server.js to serve from root directory
    Updated static file serving and index file path.

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const express = require('express');
+jsconst express = require('express');
 const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve from root instead of /public
+app.use(express.static(__dirname));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🏀 Hoop Culture Board running on http://localhost:${PORT}`);
+  console.log(`🏀 Hoop Culture Board running on port ${PORT}`);
 });
